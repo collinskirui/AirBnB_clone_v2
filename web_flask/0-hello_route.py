05730067d9faef949ace049d1b35779bbc5def13
collinskirui <collins.kirui@strathmore.edu>
2022-12-08 14:54:26 +0000

    task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_route():
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
